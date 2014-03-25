ca961cb562130514b1e2f55aa45a0e1f40140d28
jorge <jogeavi914@gmail.com>
2014-03-25 16:12:46 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenDataHacks::Application.config.secret_key_base = '68534b2d5be72cf3af971ed2d9d035a6e4d3018e3053a3f2401b29b7984ea14d3539c7427b1ddafeca56f8c6e8bbee717b42601c15d7b03757c2c5fb004cf512'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpenDataHacks::Application.config.session_store :cookie_store, key: '_open-data-hacks_session'
