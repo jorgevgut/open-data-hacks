208f5a2e2f2866ca27caa62d97935cbf7f5b4918
jorge <jogeavi914@gmail.com>
2014-03-25 19:38:51 -0600

    bootsrap y controlador prueba

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 OpenDataHacks::Application.routes.draw do
+  get "post/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
