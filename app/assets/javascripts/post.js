6a40efad9e61ccc4608b81e1906f3f4c0113adde
jorge <jogeavi914@gmail.com>
2014-04-02 19:25:30 -0600

    general changes

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,8 +1,24 @@
 //= require_tree ./libs
 
+
 init = function(){
+    var facts;
+    var regions:
+    var crime_type;
+    var crimes;
+    var time;
     
-    console.log("starting..");
+    d3.csv("/nyc-crime.csv",function(error,data){
+	if(error!=null)
+	    return;
+	facts = crossfilter(data);
+	regions = facts.dimension(function(d){return d.region});
+	crime_type = facts.dimension(function(d){return d.crime_type});
+	crimes = facts.dimension(function(d){d.crime});
+	time = facts.dimension(function(d){return d.time});
+	
+	
+    })
     
 }
 
